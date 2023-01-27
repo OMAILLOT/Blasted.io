6c6b55dacdfeabebf42e01daa0b32d8510feab08
AlexisGelin <74500014+ChronoBlasted@users.noreply.github.com>
2023-01-27 22:05:45 +0100

    map better camera lerp normalized movement

--- Blasted.io/Main.cpp
+++ Blasted.io/Main.cpp
@@ -8,8 +8,10 @@ Font font;
 CircleShape player;
 RectangleShape backgroundWorld;
 vector<RectangleShape> world;
+Sprite WorldCell;
 
 float posX, posY = 0;
+
 
 int main()
 {
@@ -30,37 +32,32 @@ int main()
 	if (!texture.loadFromFile("res/AllGridpng.png"))
 		return 1;
 
-	Sprite sprite;
-	sprite.setTexture(texture);
-	sprite.setPosition(0, 0);
+	WorldCell.setTexture(texture);
+	WorldCell.setPosition(0, 0);
 
 
 	while (window.isOpen())
 	{
 		Event event;
 		while (window.pollEvent(event)) input.InputHandler(event, window);
-		
+
 		CheckInput();
+		PlayerMovement();
 
-		player.setPosition(posX, posY);
-
-		camera.setCenter(player.getPosition().x + player.getRadius(), player.getPosition().y + player.getRadius());
-		window.setView(camera);
+		LerpCamera();
 
 		window.clear();
 
-		window.draw(backgroundWorld);
-
-
-		for (int i = 0; i < 100; i++)
+		//window.draw(backgroundWorld);
+		for (int i = 0; i < rows; i++)
 		{
-			for (int j = 0; j < 100; j++)
+			for (int j = 0; j < columns; j++)
 			{
-				sprite.setPosition(i * sprite.getLocalBounds().width, j * sprite.getLocalBounds().height);
-				window.draw(sprite);
+				WorldCell.setPosition(i * WorldCell.getLocalBounds().width, j * WorldCell.getLocalBounds().height);
+				WorldCell.move(-rows / 2 * WorldCell.getLocalBounds().width, -columns / 2 * WorldCell.getLocalBounds().height);
+				window.draw(WorldCell);
 			}
 		}
-
 
 		window.draw(player);
 
@@ -69,6 +66,61 @@ int main()
 
 	return 0;
 }
+
+void LerpCamera()
+{
+	Vector2f cameraPos = camera.getCenter();
+	Vector2f playerPos = player.getPosition();
+
+	playerPos.x += player.getRadius();
+	playerPos.y += player.getRadius();
+
+
+	cameraPos = Vector2f(lerp(cameraPos, playerPos, 0.05f));
+
+	camera.setCenter(cameraPos);
+	window.setView(camera);
+}
+
+void CheckInput() {
+	if (input.GetButton().left == true)
+	{
+		playerSpeed.x = -PLAYER_SPEED;
+
+		if (player.getPosition().x < -(rows / 2 * WorldCell.getLocalBounds().width))
+			playerSpeed.x = 0;
+	}
+	if (input.GetButton().right == true)
+	{
+		playerSpeed.x = PLAYER_SPEED;
+
+		if (player.getPosition().x > (rows / 2 * WorldCell.getLocalBounds().width) - player.getRadius() * 2)
+			playerSpeed.x = 0;
+	}
+	if (input.GetButton().up == true)
+	{
+		playerSpeed.y = -PLAYER_SPEED;
+
+		if (player.getPosition().y < -(columns / 2 * WorldCell.getLocalBounds().height))
+			playerSpeed.y = 0;
+
+	}
+	if (input.GetButton().down == true)
+	{
+		playerSpeed.y = PLAYER_SPEED;
+
+		if (player.getPosition().y > (columns / 2 * WorldCell.getLocalBounds().height) - player.getRadius() * 2)
+			playerSpeed.y = 0;
+	}
+	if (input.GetButton().attack == true)
+	{
+	}
+
+	if (input.GetButton().left == false && input.GetButton().right == false) playerSpeed.x = 0;
+	if (input.GetButton().up == false && input.GetButton().down == false) playerSpeed.y = 0;
+}
+
+#pragma region Init
 
 void InitWindow()
 {
@@ -82,13 +134,7 @@ void InitWindow()
 	options.antialiasingLevel = 8;
 }
 
-void InitPlayer()
-{
-	player.setRadius(25.f);
-	player.setFillColor(blue);
 
-	posX = -player.getRadius(), posY = -player.getRadius();
-}
 
 void InitWorld()
 {
@@ -97,6 +143,39 @@ void InitWorld()
 	backgroundWorld.setFillColor(darkGrey);
 
 }
+#pragma endregion
+
+#pragma region Player
+
+void InitPlayer()
+{
+	player.setRadius(25.f);
+	player.setFillColor(blue);
+
+	posX = -player.getRadius(), posY = -player.getRadius();
+}
+
+void PlayerMovement()
+{
+	playerSpeed = normalize(playerSpeed);
+
+	playerSpeed.x *= PLAYER_SPEED;
+	playerSpeed.y *= PLAYER_SPEED;
+
+	player.move(playerSpeed);
+}
+
+Vector2f normalize(Vector2f vector) {
+	float length = sqrt((vector.x * vector.x) + (vector.y * vector.y));
+	if (length != 0)
+		return Vector2f(vector.x / length, vector.y / length);
+	else
+		return vector;
+}
+
+#pragma endregion
+
+#pragma region Tools
 
 
 void LoadFont()
@@ -113,37 +192,10 @@ void SetText(Text& txt, String str, int size)
 	txt.setCharacterSize(size);
 }
 
-void CheckInput() {
-	if (input.GetButton().left == true)
-	{
-		posX -= PLAYER_SPEED;
-
-		/*if (posX < -(rows * squareSize / 2))
-			posX = -(rows * squareSize / 2);*/
-	}
-	if (input.GetButton().right == true)
-	{
-		posX += PLAYER_SPEED;
-
-		/*if (posX > (rows * squareSize / 2) - player.getRadius() * 2)
-			posX = (rows * squareSize / 2) - player.getRadius() * 2;*/
-	}
-	if (input.GetButton().up == true)
-	{
-		posY -= PLAYER_SPEED;
-
-		/*if (posY < -(columns * squareSize / 2))
-			posY = -(columns * squareSize / 2);*/
-
-	}
-	if (input.GetButton().down == true)
-	{
-		posY += PLAYER_SPEED;
-
-		/*if (posY > (columns * squareSize / 2) - player.getRadius() * 2)
-			posY = (columns * squareSize / 2) - player.getRadius() * 2;*/
-	}
-	if (input.GetButton().attack == true)
-	{
-	}
+Vector2f lerp(Vector2f start, Vector2f end, float percent)
+{
+	return start + (end - start) * percent;
 }
+#pragma endregion
+
+
--- Blasted.io/Main.h
+++ Blasted.io/Main.h
@@ -9,17 +9,18 @@ using namespace std;
 const int WIN_WIDTH = 1280;
 const int WIN_HEIGHT = 720;
 
-const int PLAYER_SPEED = 5;
+const int PLAYER_SPEED = 2;
+Vector2f playerSpeed(0.f, 0.f);
 
-const long rows = 64;
-const long columns = 64;
+const long rows = 32;
+const long columns = 32;
 
 const int squareSize = 48;
 const float squareOutlineThickness = 1.f;
 
 
 const Color darkGrey(168, 168, 168);
-const Color grey(184,184, 184);
+const Color grey(184, 184, 184);
 const Color lightGrey(205, 205, 205);
 
 const Color blue(0, 179, 223);
@@ -27,13 +28,13 @@ const Color blue(0, 179, 223);
 
 
 int main();
-
+void PlayerMovement();
+void LerpCamera();
 void InitWindow();
-
 void InitPlayer();
-
 void InitWorld();
-
 void LoadFont();
 void CheckInput();
 void SetText(Text& txt, String str, int size);
+Vector2f lerp(Vector2f start, Vector2f end, float percent);
+Vector2f normalize(Vector2f vector);
